46125fc66e7a7a63d2edb907c317cf2320fdca72
Sam <gitsam@samlr.com>
2024-04-28 00:51:03 +0100

    redo migrations & add admin view for event tickets

--- models/cfp.py
+++ models/cfp.py
@@ -860,8 +860,13 @@ class WorkshopProposal(Proposal):
     published_age_range = db.Column(db.String)
     published_cost = db.Column(db.String)
     published_participant_equipment = db.Column(db.String)
-    requires_ticket = db.Column(db.Boolean, default=False, nullable=False)
-    non_lottery_tickts = db.Column(db.Integer, default=5, nullable=True)
+
+    requires_ticket = db.Column(db.Boolean, default=False, nullable=True)
+    total_tickets = db.Column(db.Integer, nullable=True)
+    non_lottery_tickets = db.Column(db.Integer, default=5, nullable=True)
+
+    def has_ticket_capacity(self):
+        return (self.total_tickets - len(self.tickets)) > 0
 
 
 class YouthWorkshopProposal(WorkshopProposal):
--- models/event_tickets.py
+++ models/event_tickets.py
@@ -20,6 +20,22 @@ class EventTicket(BaseModel):
 
     id = db.Column(db.Integer, primary_key=True)
     state = db.Column(db.String, nullable=False)
-    event_id = db.Column(db.Integer, db.ForeignKey("proposal.id"), nullable=False)
+    proposal_id = db.Column(db.Integer, db.ForeignKey("proposal.id"), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    rank = db.Column(db.Integer, nullable=False)
+    rank = db.Column(db.Integer, nullable=True)
+
+    def __init__(self, user_id, proposal_id, state, rank=None):
+        self.user_id = user_id
+        self.proposal_id = proposal_id
+        self.state = state
+        self.rank = rank
+
+
+def create_ticket(user, proposal):
+    return EventTicket(user.id, proposal.id, state="ticket")
+
+
+def create_lottery_ticket(user, proposal, rank=None):
+    if not rank:
+        rank = len(user.event_tickets.all())
+    return EventTicket(user.id, proposal.id, state="entered-lottery", rank=rank)
